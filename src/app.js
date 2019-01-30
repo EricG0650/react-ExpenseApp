2c5bcea4965db50b0fb0c802532bee01d17e71d9
unknown <ericg0650@gmail.com>
2019-01-29 17:04:07 -0800

    database

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import './firebase/firebase';
+import { startSetExpenses } from './actions/expenses';
 
 const store = configureStore();
 
@@ -19,4 +21,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=>{
+  ReactDOM.render(jsx,document.getElementById('app'));
+});
---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyD3kCwatT_-52SyAMdbOT6tD4l-_Hp4dx8",
+    authDomain: "expensify-5d08c.firebaseapp.com",
+    databaseURL: "https://expensify-5d08c.firebaseio.com",
+    projectId: "expensify-5d08c",
+    storageBucket: "expensify-5d08c.appspot.com",
+    messagingSenderId: "385790957071"
+  };
+  firebase.initializeApp(config);
+
+  const database = firebase.database();
+
+export { firebase, database as default };
